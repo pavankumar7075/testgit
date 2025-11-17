04f3899fb26a90bb77400a8c75f56d1997f5df7e
pavankumar7075 <pa1kumar7075@gmail.com>
2025-11-17 18:53:52 +0530

    initial changes

---
+++ add.java
@@ -0,0 +1,11 @@
+class Add{
+    public static void main(String args[]){
+        int n = 5;
+        int m = 6;
+        int result = addTwoNumbers(n,m);
+        System.out.println(result);
+    }
+    public static int addTwoNumbers(int n, int m){
+         return n+m;
+    }
+}
